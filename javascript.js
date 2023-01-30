b77618268ee22b3d186a7ef397c7606951a5e74b
Ryan Zella <ryanmzella@gmail.com>
2023-01-30 15:19:47 -0500

    grid is adjustable, changed reset grid to clear grid

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-//TODO -- create flex container for the buttons; Create Reset Grid Function(cycle through the GridItem1-256 divs to re-assign them to background-color: white)
+/*TODO*/
 
 //create user modification at left of the screen
 const btnGridSize = document.createElement("input");
@@ -6,49 +6,47 @@ const btnResetGrid = document.createElement("input");
 
 const btnGridSizeType = btnGridSize.type = "button";
 const btnGridSizeID = btnGridSize.id = "btnGridSize";
-const btnGridSizeValue = btnGridSize.value = "Testing";
+const btnGridSizeValue = btnGridSize.value = "Grid Size";
 const btnGridSizeOnClick = btnGridSize.onclick = btnPrompt;
 
 const btnResetGridType = btnResetGrid.type = "button";
 const btnResetGridID = btnResetGrid.id = "resetGrid";
-const btnResetGridValue = btnResetGrid.value = "Reset Grid";
-//const btnResetGridOnClick = btnResetGrid.onclick = btnReset;
+const btnResetGridValue = btnResetGrid.value = "Clear Grid";
+const btnResetGridOnClick = btnResetGrid.onclick = btnReset;
 
 
 const divBtns = document.getElementById("divButtons");
 divBtns.appendChild(btnGridSize);
 divBtns.appendChild(btnResetGrid);
 
+
+
 //grab and style the container
 const container = document.getElementById("containerDiv");
 container.style.display = "inline-grid";
-container.style.gridTemplateColumns = "auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto";
+container.style.gridTemplateColumns = "auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto"; //adjust this on how big they want the grid, move to grid creation
 container.style.borderTop = "1px solid";
 container.style.borderLeft = "1px solid";
 
-//create all 16x16 divs and assign them a class of gridItem
+//generate initial 16x16 grid
 for(var i = 0; i < 256; i++)
-{
-    var div = document.createElement("div");
-    div.id = "gridItem" + (i + 1);
-    div.style.width = "16px";
-    div.style.height = "16px";
-    div.style.borderRight = "1px solid";
-    div.style.borderBottom = "1px solid";
+        {
+            var div = document.createElement("div");
+            div.className = "gridItem";
+            div.style.width = "16px";
+            div.style.height = "16px";
+            div.style.borderRight = "1px solid";
+            div.style.borderBottom = "1px solid";
+            
 
-    var gridItem = "gridItem" + (i + 1);
-    var gridItemID = document.getElementById(gridItem);
+            div.onmouseover = mouseOver;
 
-    div.onmousedown = mouseDown;
-    //div.onmouseleave = mouseOut;
+            container.appendChild(div);
+        }
 
-    
-
-    container.appendChild(div);
-}
 
 //hover function to change gridItems to black
-function mouseDown()
+function mouseOver()
 {
     this.style.backgroundColor = "black";
 }
@@ -61,18 +59,42 @@ function btnPrompt()
     if (gridSize == null || gridSize <= 0)
     {
         text = "Enter a value between 1 and 100";
+        document.getElementById("errorText").innerHTML = text;
     }
     else
     {
+        //delete existing grid
+        while (container.hasChildNodes()){
+            container.removeChild(container.children[0]);
+        }
+        
         //adjust the size of the grid
-        text = "Success";
+        for(var i = 0; i < (gridSize * gridSize); i++)
+        {
+            container.style.gridTemplateColumns = "auto ".repeat(gridSize);
+            var div = document.createElement("div");
+            div.className = "gridItem";
+            div.style.width = "16px";
+            div.style.height = "16px";
+            div.style.borderRight = "1px solid";
+            div.style.borderBottom = "1px solid";
+            
+
+            div.onmouseover = mouseOver;
+
+            container.appendChild(div);
+        }
     }
-    document.getElementById("errorText").innerHTML = text;
+    
 
 }
 
-//reset grid function
-function resetGrid()
+//reset grid to white
+function btnReset()
 {
-
+    var gridItems = document.getElementsByClassName("gridItem");
+    for (var i = 0; i < gridItems.length; ++i)
+    {
+        gridItems[i].style.backgroundColor = "white";
+    }
 }
